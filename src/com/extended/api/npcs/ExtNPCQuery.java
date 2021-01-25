86da7e7ac0db4a330419119c67e502c4889dc8a6
DefSmile <notsmile@protonmail.com>
2021-01-25 01:22:47 -0500

    Added some more methods to have more options for querying.

--- src/com/extended/api/npcs/ExtNPCQuery.java
+++ src/com/extended/api/npcs/ExtNPCQuery.java
@@ -6,6 +6,15 @@ import com.epicbot.api.shared.entity.NPC;
 public class ExtNPCQuery {
 
     public NPC getNearestNPC(String name, APIContext ctx) {
+        return ctx.npcs().query().nameMatches(name).results().nearest();
+    }
+    public NPC getNearestNPC(String name, String action, APIContext ctx) {
+        return ctx.npcs().query().nameMatches(name).actions(action).results().nearest();
+    }
+    public NPC getNotInCombatNPC(String name, APIContext ctx) {
         return ctx.npcs().query().nameMatches(name).notInCombat().animation(-1).results().nearest();
+    }
+    public NPC getNotInCombatNPC(String name, String action ,APIContext ctx) {
+        return ctx.npcs().query().nameMatches(name).actions(action).notInCombat().animation(-1).results().nearest();
     }
 }
