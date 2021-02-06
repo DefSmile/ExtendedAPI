2d28a3cfe7cdc93dd33ab21a9a4a1e43df1c8229
DefSmile <notsmile@protonmail.com>
2021-02-06 10:38:23 -0500

    Made majority of the queries return .nearest() to test if you can still null check w/ the query returning an actual NPC.

--- src/com/extended/api/npcs/ExtNPCQuery.java
+++ src/com/extended/api/npcs/ExtNPCQuery.java
@@ -8,35 +8,23 @@ import com.epicbot.api.shared.query.result.LocatableEntityQueryResult;
 public class ExtNPCQuery {
 
     // Look up through names
-    public LocatableEntityQueryResult<NPC> getNearestNPC(String name, APIContext ctx) {
-        return ctx.npcs().query().nameMatches(name).results();
-    }
-    public LocatableEntityQueryResult<NPC> getNearestNPC(String name, String action, APIContext ctx) {
-        return ctx.npcs().query().nameMatches(name).actions(action).results();
-    }
-    public LocatableEntityQueryResult<NPC> getNotInCombatNPC(String name, APIContext ctx) {
-        return ctx.npcs().query().nameMatches(name).notInCombat().animation(-1).results();
-    }
-    public LocatableEntityQueryResult<NPC> getNotInCombatNPC(String name, String action ,APIContext ctx) {
-        return ctx.npcs().query().nameMatches(name).actions(action).notInCombat().animation(-1).results();
-    }
-    public NPCQueryBuilder getNPC(String name, APIContext ctx) {
-        return ctx.npcs().query().nameMatches(name);
-    }
-    // Look up through IDs
-    public LocatableEntityQueryResult<NPC> getNearestNPC(int id, APIContext ctx) {
-        return ctx.npcs().query().id(id).results();
-    }
-    public LocatableEntityQueryResult<NPC> getNearestNPC(int id, String action, APIContext ctx) {
-        return ctx.npcs().query().id(id).actions(action).results();
-    }
-    public LocatableEntityQueryResult<NPC> getNotInCombatNPC(int id, APIContext ctx) {
-        return ctx.npcs().query().id(id).notInCombat().animation(-1).results();
-    }
-    public LocatableEntityQueryResult<NPC> getNotInCombatNPC(int id, String action ,APIContext ctx) {
-        return ctx.npcs().query().id(id).actions(action).notInCombat().animation(-1).results();
-    }
-    public NPCQueryBuilder getNPC(int id, APIContext ctx) {
-        return ctx.npcs().query().id(id);
-    }
+    public NPC getNearestNPC(String name, APIContext ctx) { return ctx.npcs().query().nameMatches(name).results().nearest(); }
+
+    public NPC getNearestNPC(String name, String action, APIContext ctx) { return ctx.npcs().query().nameMatches(name).actions(action).results().nearest(); }
+
+    public NPC getNotInCombatNPC(String name, APIContext ctx) { return ctx.npcs().query().nameMatches(name).notInCombat().animation(-1).results().nearest(); }
+
+    public NPC getNotInCombatNPC(String name, String action ,APIContext ctx) { return ctx.npcs().query().nameMatches(name).actions(action).notInCombat().animation(-1).results().nearest(); }
+
+    public NPCQueryBuilder getNPC(String name, APIContext ctx) { return ctx.npcs().query().nameMatches(name); }
+
+    public NPC getNearestNPC(int id, APIContext ctx) { return ctx.npcs().query().id(id).results().nearest(); }
+
+    public NPC getNearestNPC(int id, String action, APIContext ctx) { return ctx.npcs().query().id(id).actions(action).results().nearest(); }
+
+    public NPC getNotInCombatNPC(int id, APIContext ctx) { return ctx.npcs().query().id(id).notInCombat().animation(-1).results().nearest(); }
+
+    public NPC getNotInCombatNPC(int id, String action ,APIContext ctx) { return ctx.npcs().query().id(id).actions(action).notInCombat().animation(-1).results().nearest(); }
+
+    public NPCQueryBuilder getNPC(int id, APIContext ctx) { return ctx.npcs().query().id(id); }
 }
