4291a98ae5afe2f00696c8ca5df4aebbac3b8195
DefSmile <notsmile@protonmail.com>
2021-02-06 16:13:28 -0500

    Revert "Made all methods static methods."
    This reverts commit 9222dcc4

--- src/com/extended/api/NPC/ExtNPCQuery.java
+++ src/com/extended/api/NPC/ExtNPCQuery.java
@@ -6,23 +6,23 @@ import com.epicbot.api.shared.query.NPCQueryBuilder;
 
 public class ExtNPCQuery {
     
-    public static NPC getNearestNPC(String name, APIContext ctx) { return ctx.npcs().query().nameMatches(name).results().nearest(); }
+    public NPC getNearestNPC(String name, APIContext ctx) { return ctx.npcs().query().nameMatches(name).results().nearest(); }
 
-    public static NPC getNearestNPC(String name, String action, APIContext ctx) { return ctx.npcs().query().nameMatches(name).actions(action).results().nearest(); }
+    public NPC getNearestNPC(String name, String action, APIContext ctx) { return ctx.npcs().query().nameMatches(name).actions(action).results().nearest(); }
 
-    public static NPC getNotInCombatNPC(String name, APIContext ctx) { return ctx.npcs().query().nameMatches(name).notInCombat().animation(-1).results().nearest(); }
+    public NPC getNotInCombatNPC(String name, APIContext ctx) { return ctx.npcs().query().nameMatches(name).notInCombat().animation(-1).results().nearest(); }
 
-    public static NPC getNotInCombatNPC(String name, String action ,APIContext ctx) { return ctx.npcs().query().nameMatches(name).actions(action).notInCombat().animation(-1).results().nearest(); }
+    public NPC getNotInCombatNPC(String name, String action ,APIContext ctx) { return ctx.npcs().query().nameMatches(name).actions(action).notInCombat().animation(-1).results().nearest(); }
 
-    public static NPCQueryBuilder getNPC(String name, APIContext ctx) { return ctx.npcs().query().nameMatches(name); }
+    public NPCQueryBuilder getNPC(String name, APIContext ctx) { return ctx.npcs().query().nameMatches(name); }
 
-    public static NPC getNearestNPC(int id, APIContext ctx) { return ctx.npcs().query().id(id).results().nearest(); }
+    public NPC getNearestNPC(int id, APIContext ctx) { return ctx.npcs().query().id(id).results().nearest(); }
 
-    public static NPC getNearestNPC(int id, String action, APIContext ctx) { return ctx.npcs().query().id(id).actions(action).results().nearest(); }
+    public NPC getNearestNPC(int id, String action, APIContext ctx) { return ctx.npcs().query().id(id).actions(action).results().nearest(); }
 
-    public static NPC getNotInCombatNPC(int id, APIContext ctx) { return ctx.npcs().query().id(id).notInCombat().animation(-1).results().nearest(); }
+    public NPC getNotInCombatNPC(int id, APIContext ctx) { return ctx.npcs().query().id(id).notInCombat().animation(-1).results().nearest(); }
 
-    public static NPC getNotInCombatNPC(int id, String action ,APIContext ctx) { return ctx.npcs().query().id(id).actions(action).notInCombat().animation(-1).results().nearest(); }
+    public NPC getNotInCombatNPC(int id, String action ,APIContext ctx) { return ctx.npcs().query().id(id).actions(action).notInCombat().animation(-1).results().nearest(); }
 
-    public static NPCQueryBuilder getNPC(int id, APIContext ctx) { return ctx.npcs().query().id(id); }
+    public NPCQueryBuilder getNPC(int id, APIContext ctx) { return ctx.npcs().query().id(id); }
 }
